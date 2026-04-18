a47c36e9fa61aa0dee28a8883440338b26f71129
ryujionagata1020 <skr.eiden1020@gmail.com>
2026-04-19 01:59:47 +0900

    google広告挿入

--- app.js
+++ app.js
@@ -100,6 +100,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user || null;
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.navByGrade = _navByGrade;
+  res.locals.canonicalUrl = 'https://qc-portals.com' + req.path;
   next();
 });
 
